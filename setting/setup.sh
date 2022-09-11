83a1086c4dfe6d17360fc1fa318d7e9cc8b569c2
jo-kwsm <ytk.kwsm@gmail.com>
2022-09-11 20:39:29 +0900

    [Update] day03

--- setting/setup.sh
+++ setting/setup.sh
@@ -2,3 +2,6 @@ source edksetup.sh
 cp ./original_setting/target.txt ./Conf/target.txt
 build
 cp ./Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ./output/BOOTX64.EFI
+source $HOME/osbook/devenv/buildenv.sh
+cd /root/kernel
+make
